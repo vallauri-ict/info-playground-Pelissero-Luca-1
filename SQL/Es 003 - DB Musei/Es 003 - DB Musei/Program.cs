95e490bc1e97d351cb7fa270e6cb4349880f04f3
Luca <l.pelissero.1360@vallauri.edu>
2021-09-30 20:53:17 +0200

    Updated esercizi mancanti

---
+++ SQL/Es 003 - DB Musei/Es 003 - DB Musei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es_003___DB_Musei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SQL/Es 003 - DB Musei/Es 003 - DB Musei/createTablesMusei.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Musei(
+	NomeMuseo VARCHAR(30) NOT NULL PRIMARY KEY,
+	Citta VARCHAR(30)
+)
+
+CREATE TABLE Artisti(
+	NomeA varchar(20) PRIMARY KEY NOT NULL,
+	Nazionalita varchar(3) NOT NULL
+);
+
+CREATE TABLE Opere(
+	Codice int PRIMARY KEY NOT NULL,
+	Titolo varchar(30) NOT NULL,
+	NomeMuseo varchar(30) NOT NULL,
+	NomeA varchar(20) NOT NULL,
+	CONSTRAINT [FK_Opere_ToTable] FOREIGN KEY ([NomeMuseo]) REFERENCES [Musei]([NomeMuseo]),
+	CONSTRAINT [FK_Opere_ToTable_1] FOREIGN KEY ([NomeA]) REFERENCES [Artisti]([NomeA])
+);
+
+CREATE TABLE Personaggi (
+	Personaggio varchar(30) PRIMARY KEY NOT NULL,
+	Codice int NOT NULL,
+	CONSTRAINT [FK_Opere_ToTable_2] FOREIGN KEY ([Codice]) REFERENCES [Opere]([Codice])
+);
