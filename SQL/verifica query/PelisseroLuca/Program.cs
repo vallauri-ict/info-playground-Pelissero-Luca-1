6bf13111acb560c2b502cb7fab3811abc4384657
Luca <l.pelissero.1360@vallauri.edu>
2021-10-23 18:37:18 +0200

    Prima verifica SQL

---
+++ SQL/verifica query/PelisseroLuca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PelisseroLuca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SQL/verifica query/query.sql
@@ -0,0 +1,39 @@
+﻿/* 1 */
+SELECT f.titolo
+FROM Film f
+WHERE f.regista = (SELECT f1.regista FROM Film f1 WHERE f1.titolo = 'Casablanca')
+
+/* 2 */
+SELECT DISTINCT f.titolo
+FROM Film f, Recita r, Attori a
+WHERE a.codAttore = r.codAttore 
+AND r.codFilm=f.codFilm
+AND a.nome='S. Loren' OR a.nome='M. Mastroianni'
+
+/* 3 */
+SELECT DISTINCT f.titolo, f.genere
+FROM Film f, Sale s, Proiezioni p
+WHERE f.codFilm = p.codFilm 
+AND p.codSala = s.codSala
+AND p.dataProiezione = '25-12-2004'
+
+/* 4 */
+SELECT f.titolo, f.regista
+FROM Film f, Attori a, Recita r
+WHERE a.codAttore=r.codAttore
+AND f.codFilm=r.codFilm
+GROUP BY f.titolo, f.regista
+HAVING COUNT(*)<6
+
+/* 5 */
+SELECT f.titolo
+FROM Film f, Proiezioni p
+WHERE f.codFilm=p.codFilm
+AND NOT EXISTS(SELECT * FROM Sale s	WHERE s.codSala=p.codFilm AND s.citta='Pisa')
+
+/* 6 */
+SELECT DISTINCT a.nome 
+FROM Attori a, Recita r
+WHERE a.codAttore=r.codAttore
+AND a.nazionalita='IT'
+AND NOT EXISTS(SELECT * FROM Film F WHERE f.codFilm=r.codFilm AND f.regista='Fellini')
